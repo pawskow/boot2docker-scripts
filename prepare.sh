ba93cf55cfc387271b343502ad92bb8361215283
Pawel Skowronski <pawel.skowronski@s3group.com>
2015-12-03 14:53:30 +0100

    fixes

--- prepare.sh
+++ prepare.sh
@@ -7,7 +7,7 @@ BOOT2DOCKER_PATH=/var/lib/boot2docker
 
 cp -r $SCRIPTPATH/boot2docker/* $BOOT2DOCKER_PATH/
 
-cmod +x $BOOT2DOCKER_PATH/bootlocal.sh
-cmod +x $BOOT2DOCKER_PATH/mount.sh
-cmod +x $BOOT2DOCKER_PATH/scripts/add_container.sh
-cmod +x $BOOT2DOCKER_PATH/add_mount.sh
+chmod +x $BOOT2DOCKER_PATH/bootlocal.sh
+chmod +x $BOOT2DOCKER_PATH/mount.sh
+chmod +x $BOOT2DOCKER_PATH/scripts/add_container
+chmod +x $BOOT2DOCKER_PATH/scripts/add_mount
